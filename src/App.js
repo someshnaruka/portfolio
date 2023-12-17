8094a98173b073e459ff1e5d504b997d272219a7
someshnaruka <82537722+someshnaruka@users.noreply.github.com>
2023-12-17 19:20:50 +0530

    added resize on font change function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 
 
@@ -16,9 +16,81 @@ function App() {
   useEffect(() => {
     window.history.scrollRestoration = 'manual'
   }, []);
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
+  const [fontSize, setFontSize] = useState();
+  const [check, setcheck] = useState(false);
+
+  const observedDiv = useRef(null);
+
+  useEffect(() => {
+    if (!observedDiv.current) {
+      return;
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+      if (observedDiv.current.offsetWidth !== width) {
+        setWidth(observedDiv.current.offsetWidth);
+      }
+      if (observedDiv.current.offsetHeight !== height) {
+        setHeight(observedDiv.current.offsetHeight);
+      }
+    });
+
+    resizeObserver.observe(observedDiv.current);
+
+    return function cleanup() {
+      resizeObserver.disconnect();
+    };
+  }, [observedDiv.current]);
+
+  useEffect(() => {
+    if (!check) {
+      const calculateFontSize = () => {
+        // Example: Font size is 5% of the average of width and height
+        const calculatedFontSize = 0.05 * ((width + height) / 2);
+        console.log("reahcer here");
+        // If calculated font size is greater than 16px, adjust body font size
+        if (calculatedFontSize > 16) {
+          const diff = calculatedFontSize - 16;
+          setFontSize(calculatedFontSize);
+          console.log("reahcer  if here");
+          setcheck(true);
+          // Update body font size
+          document.body.style.fontSize = `16px`;
+          document.body.style.fontSize = `calc(16px - ${diff}px)`;
+        } else if (calculatedFontSize < 16) {
+            const diff =16 - calculatedFontSize;
+            setFontSize(calculatedFontSize);
+            console.log("reahcer  if here");
+            setcheck(true);
+            // Update body font size
+            document.body.style.fontSize = `16px`;
+            document.body.style.fontSize = `calc(16px + ${diff}px)`;
+          }
+        else {
+          setFontSize(calculatedFontSize);
+        }
+      };
+
+      // Initial calculation
+      calculateFontSize();
+
+      // Recalculate on window resize
+      const handleResize = () => {
+        calculateFontSize();
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [width, height]);
   return (
 
-    <div className="App">
+    <div className="App" ref={observedDiv}>
         <Toaster></Toaster>
       <Navbar></Navbar>
      
